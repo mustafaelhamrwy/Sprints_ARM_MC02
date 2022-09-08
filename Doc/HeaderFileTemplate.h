1b1dafacb4ca3864fd1b43568b96fc278434e429
mustafaelhamrawy$ git push gitk --all & git config --global user.name mustafaelhamrawy <engmustafaawad22@gmail.com>
2022-09-08 17:39:08 +0200

    init commit

--- Doc/HeaderFileTemplate.h
+++ Doc/HeaderFileTemplate.h
@@ -2,14 +2,14 @@
 
  *  FILE DESCRIPTION
  *  -------------------------------------------------------------------------------------------------------------------
- *         File:  Platform_Types.h
+ *         File:  <Write File Name>
  *       Module:  -
  *
- *  Description:  Contains types that dependent on platform - CortexM4     
+ *  Description:  <Write File DESCRIPTION here>     
  *  
  *********************************************************************************************************************/
-#ifndef PLATFORM_TYPES_H
-#define PLATFORM_TYPES_H
+#ifndef <FILE_NAME_H>
+#define <FILE_NAME_H>
 
 /**********************************************************************************************************************
  * INCLUDES
@@ -19,27 +19,7 @@
 /**********************************************************************************************************************
  *  GLOBAL CONSTANT MACROS
  *********************************************************************************************************************/
-#define WORD_LENGTH_BITS      32u
-#define WORD_LENGTH_BYTES     4u
-#define MSB_FIRST             0u    /* big endian bit ordering */
-#define LSB_FIRST        1u    /* little endian bit ordering */
 
-#define HIGH_BYTE_FIRST  0u    /* big endian byte ordering */
-#define LOW_BYTE_FIRST   1u    /* little endian byte ordering */
-
-#ifndef TRUE
-   #define TRUE   1u
-#endif
-
-#ifndef FALSE
-   #define FALSE  0u
-#endif
-
-#define ENABLE    1u
-#define DISABLE   0u
-
-#define CPU_BIT_ORDER    LSB_FIRST        /*little endian bit ordering*/
-#define CPU_BYTE_ORDER   LOW_BYTE_FIRST   /*little endian byte ordering*/
 
 /**********************************************************************************************************************
  *  GLOBAL FUNCTION MACROS
@@ -49,22 +29,7 @@
 /**********************************************************************************************************************
  *  GLOBAL DATA TYPES AND STRUCTURES
  *********************************************************************************************************************/
-typedef unsigned char         boolean;       /*        TRUE .. FALSE           */
 
-typedef signed char           sint8;         /*        -128 .. +127            */
-typedef unsigned char         uint8;         /*           0 .. 255             */
-typedef signed short          sint16;        /*      -32768 .. +32767          */
-typedef unsigned short        uint16;        /*           0 .. 65535           */
-typedef signed long           sint32;        /* -2147483648 .. +2147483647     */
-typedef unsigned long         uint32;        /*           0 .. 4294967295      */
-
-typedef float                 float32;
-typedef double                float64;
-
-#ifdef PLATFORM_SUPPORT_SINT64_UINT64 /*Valid only for gnu and C99 */
-typedef signed    long long  sint64;   /* -9223372036854775808 .. 9223372036854775807      */
-typedef unsigned  long long  uint64;   /*                    0 .. 18446744073709551615     */
-#endif
 
 /**********************************************************************************************************************
  *  GLOBAL DATA PROTOTYPES
@@ -76,7 +41,7 @@ typedef unsigned  long long  uint64;   /*                    0 .. 18446744073709
  *********************************************************************************************************************/
 
  
-#endif  /* PLATFORM_TYPES_H */
+#endif  /* FILE_NAME_H */
 
 /**********************************************************************************************************************
  *  END OF FILE: Std_Types.h
